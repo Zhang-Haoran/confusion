8fbe7648a178b9299683c7c4d4faabdf829ad109
Zhang-Haoran <hzha278@student.monash.edu>
2020-12-30 18:01:49 +1100

    component part 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+
 import {Navbar, NavbarBrand} from "reactstrap";
+import Menu from "./compoents/MenuComponent";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">
@@ -11,6 +12,7 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
+        <Menu/>
     </div>
   );
 }
